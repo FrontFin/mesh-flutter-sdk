2f8e1a6f2d8bf3404bbb8f2a880bf70b4dae327b
Miloš Jokić <miloshjokic@gmail.com>
2025-10-28 14:59:26 +0100

    example app

---
+++ example/lib/main.dart
@@ -0,0 +1,110 @@
+// ignore_for_file: avoid_print
+import 'package:flutter/material.dart';
+import 'package:mesh_sdk/mesh_sdk.dart';
+
+void main() {
+  runApp(const MeshExampleApp());
+}
+
+class MeshExampleApp extends StatelessWidget {
+  const MeshExampleApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      localizationsDelegates: MeshLocalizations.localizationsDelegates,
+      theme: ThemeData.from(
+        colorScheme: const ColorScheme.dark(primary: Color(0xFFFAFF6E)),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    final linkToken = _textController.text.trim();
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Mesh Example App')),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Center(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                TextField(
+                  controller: _textController,
+                  decoration: const InputDecoration(
+                    labelText: 'Link Token',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(8)),
+                    ),
+                  ),
+                  onChanged: (value) => setState(() {}),
+                ),
+                const SizedBox(height: 32),
+                FilledButton(
+                  onPressed: linkToken.isEmpty
+                      ? null
+                      : () => _showMeshLinkPage(linkToken),
+                  child: const Text('Start'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _showMeshLinkPage(String linkToken) async {
+    final result = await MeshSdk.show(
+      context,
+      configuration: MeshConfiguration(
+        language: 'en',
+        integrationAccessTokens: const [
+          IntegrationAccessToken(
+            accessToken: 'token',
+            accountId: 'id',
+            accountName: 'name',
+            brokerName: 'broker',
+            brokerType: 'type',
+          ),
+        ],
+        linkToken: linkToken,
+        onEvent: (event) {
+          print('Mesh event: $event');
+        },
+        onExit: (errorType) {
+          print('Mesh exit: $errorType');
+        },
+        onIntegrationConnected: (integration) {
+          print('Integration connected: $integration');
+        },
+        onTransferFinished: (transfer) {
+          print('Transfer finished: $transfer');
+        },
+      ),
+    );
+
+    switch (result) {
+      case MeshSuccess():
+        print('Mesh link finished successfully');
+      case MeshError():
+        print('Mesh link error: ${result.type}');
+    }
+  }
+}
