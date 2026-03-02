85fe9295ae0d583d70db56f3cb3f026e8b2931c8
Vitalii Movchan <vitaliy.movchan@meshconnect.com>
2026-03-02 23:24:31 +0200

    PRG-2119 added es, pt locales

---
+++ lib/src/l10n/mesh_localizations_pt.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'mesh_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Portuguese (`pt`).
+class MeshLocalizationsPt extends MeshLocalizations {
+  MeshLocalizationsPt([String locale = 'pt']) : super(locale);
+
+  @override
+  String get exitDialogTitle => 'Tem certeza de que deseja sair?';
+
+  @override
+  String get exitDialogMessage => 'Seu progresso será perdido.';
+
+  @override
+  String get exit => 'Sair';
+
+  @override
+  String get cancel => 'Cancelar';
+}
