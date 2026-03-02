0eee4e8b4a760c41df82eee4c0f05c59406853f6
Vitalii Movchan <vitaliy.movchan@meshconnect.com>
2026-03-02 22:54:24 +0200

    PRG-2119 fallback to en

--- lib/src/extension/context.dart
+++ lib/src/extension/context.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mesh_sdk_flutter/src/l10n/mesh_localizations.dart';
+import 'package:mesh_sdk_flutter/src/l10n/mesh_localizations_en.dart';
 
 extension BuildContextX on BuildContext {
   MeshLocalizations get l10n {
-    final l10n = MeshLocalizations.of(this);
-    if (l10n == null) {
-      throw Exception(
-        '$MeshLocalizations not found. '
-        'Did you forget to add it to MaterialApp.localizationsDelegates?',
-      );
-    }
-
-    return l10n;
+    return MeshLocalizations.of(this) ?? MeshLocalizationsEn('en');
   }
 }
